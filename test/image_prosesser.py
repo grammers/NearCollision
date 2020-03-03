bdba15698a992ae2f5dee398ab6a3d2c7d54ccb6
grammers <samuel_karlsson@outlook.com>
2020-03-03 13:49:52 +0100

    implemented ros nod for single steam

---
+++ test/image_prosesser.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import cv2
+
+from collections import deque
+from torchvision.transforms import transforms
+from cv_bridge import CvBridge, CvBridgeError
+
+from sensor_msgs.msg import Image
+
+
+class Image_prosesser:
+    
+    def __init__(self, stream_len):
+        self.buf_size = stream_len
+        self.bridge = CvBridge()
+        self.stack_images = deque(maxlen = self.buf_size)
+
+    def preprocess(self, image):
+        cv_img = self.bridge.imgmsg_to_cv2(image, "bgr8")
+        cv_img = cv2.resize(cv_img, (224, 224))
+
+        normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], 
+        std=[0.229, 0.224, 0.225])
+        
+        transform = transforms.Compose(
+        [transforms.ToTensor(), normalize])
+        
+        cv_img = transform(cv_img)
+
+        ## unsqueeze it to become torch.Size([1, 3, 224, 224])
+        cv_img = cv_img.unsqueeze(0)
+        cv_img = cv_img.float().cuda()
+        
+        self.stack_images.append(cv_img)
+
+    def is_filed(self):
+        return len(self.stack_images) == self.buf_size
+
+    def stack(self):
+        return self.stack_images
+        
---
+++ test/singel_stream_prediction.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import image_prosesser
+from collections import deque 
+
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float32
+
+import torch
+import torchvision.models as models
+import torch.nn as nn 
+
+mean = 2.2997543581616484
+class net_loader:
+
+    def __init__(self):
+        self.model = models.vgg16(pretrained=True)
+        self.num_final_in = self.model.classifier[-1].in_features
+        self.NUM_CLASSES = 20
+        self.model.classifier[-1] = nn.Linear(
+            self.num_final_in, self.NUM_CLASSES) ## Regressed output
+    #    self.model = nn.Linear(self.num_final_in, NUM_CLASSES)
+        
+        self.load_weights('/home/hexa/catkin_workspaces/catkin_samuel/src/nearCollision/data/trained_models/vgg_on_voc800')
+
+        self.load_pred()
+        self.load_weights('/home/hexa/catkin_workspaces/catkin_samuel/src/nearCollision/data/trained_models/SingleImage6s_047')
+        
+        self.model = self.model.cuda()
+        self.model.eval()
+    
+    def load_pred(self):
+        self.model.classifier[-1] = nn.Linear(
+        self.num_final_in, self.NUM_CLASSES)
+        num_features = self.model.classifier[6].in_features
+        features = list(self.model.classifier.children())[:-1]
+        
+        features.extend([
+            nn.Linear(num_features,2048),
+            nn.ReLU(), nn.Linear(2048,1)
+        ])
+        self.model.classifier = nn.Sequential(*features)
+
+    def load_weights(self, model_path):
+        self.model.load_state_dict(
+            torch.load(model_path))
+
+    def feed_net(self, stack_img):
+        return self.model(stack_img.pop())[0].data + mean
+        
+        
+class ROS_runner:
+    
+    def __init__(self):
+        self.image_prosesser = image_prosesser.Image_prosesser(1)
+
+        self.network = net_loader()
+        #ros setup
+        self.image_sub = rospy.Subscriber(
+        "/usb_cam/image_raw", Image, self.callback)
+        
+        self.time_pub = rospy.Publisher(
+        'near_collision_time', Float32, queue_size = 10)
+
+        print('setup complet')
+
+    def callback(self, data):
+        self.image_prosesser.preprocess(data) 
+       
+        if (self.image_prosesser.is_filed()):
+            t = self.network.feed_net(self.image_prosesser.stack())
+        else:
+            t = 1000
+
+        self.time_pub.publish(t)
+        print(t)
+
+def main():
+    ros_runner = ROS_runner()
+    rospy.init_node('run_on_cam', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllwindows()
+
+if __name__ == '__main__':
+    main()
